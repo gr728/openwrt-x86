5f17b642216e5d1dc5bb4bbc86da6db2d3793c26
gr728 <62072234+gr728@users.noreply.github.com>
2020-12-16 12:38:55 +0800

    build openwrtx-86

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,9 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.20.5/g' package/base-files/files/bin/config_generate
+echo '添加登录密码密文'
+sed -i 's/root::0:0:99999:7:::/root:$1$FHLZsCKT$LvLemPCPOEofaV5pyBWhP0:0:0:99999:7:::/g'  package/base-files/files/etc/shadow
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
